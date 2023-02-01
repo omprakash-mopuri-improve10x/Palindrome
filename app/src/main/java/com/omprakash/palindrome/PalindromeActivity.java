ba12f1bf73c9f728c8ee4b6e8e8e4921c31030bb
Omprakash <omprakash.mopuri@improve10x.com>
2023-02-01 14:55:25 +0530

    Implemented the checkPalindrome() method in PalindromeActivity.java and modified the findPalindrome() method

--- app/src/main/java/com/omprakash/palindrome/PalindromeActivity.java
+++ app/src/main/java/com/omprakash/palindrome/PalindromeActivity.java
@@ -3,18 +3,29 @@ package com.omprakash.palindrome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.omprakash.palindrome.databinding.ActivityPalindromeBinding;
 
 public class PalindromeActivity extends AppCompatActivity implements PalindromeView {
+
+    private ActivityPalindromeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_palindrome);
+        binding = ActivityPalindromeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        checkPalindrome();
     }
 
     @Override
     public void checkPalindrome() {
-
+        binding.checkPalindromeBtn.setOnClickListener(v -> {
+            String input = binding.inputTxt.getText().toString();
+            String output = new PalindromeControllerImpl().findPalindrome(input);
+            Toast.makeText(this, output, Toast.LENGTH_SHORT).show();
+        });
     }
 
     @Override
--- app/src/main/java/com/omprakash/palindrome/PalindromeControllerImpl.java
+++ app/src/main/java/com/omprakash/palindrome/PalindromeControllerImpl.java
@@ -4,10 +4,11 @@ public class PalindromeControllerImpl implements PalindromeController{
     @Override
     public String findPalindrome(String input) {
        String reverse = "";
+       input = input.replace(" ", "");
        for (int i = input.length() - 1; i >= 0; i--) {
            reverse = reverse + input.charAt(i);
        }
-       if (input.equalsIgnoreCase(reverse)) {
+       if (input.equals(reverse)) {
            return input + " is a Palindrome";
        } else {
            return input + " is not a Palindrome";
